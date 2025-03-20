9dde9160ebfde1cce5a3b863aa8af8905ae76641
nxg02772 <manikalamanoj.kumar@nxp.com>
2025-03-20 18:22:30 +0530

    some few changes in data validation

--- src/entity/artifact_entity.py
+++ src/entity/artifact_entity.py
@@ -9,3 +9,9 @@ class DataIngestionArtifact:
 class DataValidationArtifact:
     validattion_status:bool
     validation_report_path:str
+
+@dataclass
+class DataTransformationArtifact:
+    transformed_train_file_path:str
+    transformed_test_file_path:str
+    transformed_object_file_path:str
